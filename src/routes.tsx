763ab2aa5b26b0dacf3050385b4bfcddd22bc880
Vinícius Rajá <viniciusrfl@hotmail.com>
2020-08-07 18:38:53 -0300

    add: routes

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+import Landing from 'pages/Landing'
+import TeacherForm from 'pages/TeacherForm'
+import TeacherList from 'pages/TeacherList'
+
+export default function Routes(){
+    return(
+        <BrowserRouter>
+        <Route path="/" exact component={Landing}/>
+        <Route path="/study" component={TeacherList}/>
+        <Route path="/give-classes" component={TeacherForm}/>
+        </BrowserRouter>
+    )
+}
